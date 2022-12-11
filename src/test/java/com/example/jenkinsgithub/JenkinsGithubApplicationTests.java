6a71a25683e201d4276a146e180b446b02c13c05
신형섭 <brothersup@kakao.com>
2022-12-11 22:44:46 +0900

    add project

---
+++ src/test/java/com/example/jenkinsgithub/JenkinsGithubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jenkinsgithub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsGithubApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
